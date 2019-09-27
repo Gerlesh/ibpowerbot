a57a82798b297f73f5a55ff4613e645cd5185c77
Gerlesh <55896914+Gerlesh@users.noreply.github.com>
2019-09-27 18:35:53 -0400

    Removed obsolete code

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// server.js
-// where your node app starts
-
-// init project
-const express = require('express');
-const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
 const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
