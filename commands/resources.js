2c9d0cc397dc78e3b097987e6c044e29269e5c71
Gerlesh <55896914+Gerlesh@users.noreply.github.com>
2019-09-27 19:50:31 -0400

    Update resources.js

--- commands/resources.js
+++ commands/resources.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const resources = require('../resources.json');
 
 module.exports = {
 	name: 'resources', //Name of command
@@ -8,14 +9,12 @@ module.exports = {
 	guildOnly: false, //Server only? (true/false)
 	cooldown: 3, //Cooldown in seconds for command
 	aliases: ['rs', 'documents', 'docs'], //Other possible ways to call command, written in '' marks and separated by ,
-  deleteMessage: false,
+	deleteMessage: false,
 	execute(message, args) {
-		const resourcesList = new Discord.RichEmbed().setTitle('**Resources**').setDescription('The following links contain useful IB Resources').setColor('#62D0F6')	//Create embed template
-			.addField('IB Documents (default)','https://www.ib.redditor.website/')																						// |
-			.addField('IBO Discord','https://discord.gg/ibo')																											// |
-			.addField('IB Resource Resource Repository','https://www.ibdocuments.com/ResourcesRepository.html')															// | Add fields for each link
-			.addField('MPSJ IB Google Drive','https://drive.google.com/drive/folders/1FaU2rxN9Jf2BJOe3Ft673mQjKGy2znQu?usp=sharing')									// |
-			.addField('IBO Subreddit','https://www.reddit.com/r/IBO/');																									// |
+		const resourcesList = new Discord.RichEmbed().setTitle('**Resources**').setDescription('The following links contain useful Resources').setColor('#62D0F6')	//Create embed template
+		for (let [name,link] of resources.entries()) {
+			resourcesList.addField(name,link);
+		};
 		message.channel.send(resourcesList); //Send embed
 	}
 };
