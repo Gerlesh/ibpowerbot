8b5d81a4454a327e2d5d615354ad569dbb3c0001
Gerlesh <55896914+Gerlesh@users.noreply.github.com>
2019-10-05 23:14:08 -0400

    Update bot

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
-@echo off
-
-title IB Test Bot
-echo IB Test Bot
-cd C:\Users\Owner\Documents\github\ibpowerbot
-node server.js
-pause
+@echo off 
+title IB Power Bot 
+echo /--------------\ 
+echo   IB Power Bot 
+echo \--------------/ 
+cd C:\Users\Owner\Documents\github\ibpowerbot\ 
+:start 
+node server.js 
+goto :start 
