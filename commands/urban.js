0b910c6846dfbf1a34a7ca12e14cf91b296126ff
Gerlesh <cjplanelles@gmail.com>
2019-10-18 14:01:52 -0400

    Bug fix

--- commands/urban.js
+++ commands/urban.js
@@ -12,28 +12,29 @@ module.exports = {
 	execute(message, args) {
 		if (args.length > 0) {
 			urban(args.join(' ')).first(function(json) {
-				const definition = new Discord.RichEmbed()
-					.setTitle(json.word)
-					.addField('Definition:',json.definition.replace(/\[\]/g,''))
-					.addField('Example:',json.example.replace(/\[\]/g,''))
-					.setTimestamp(json.written_on.substring(0,json.written_on.indexOf('T')-1).replace(/-/gi,' '))
-					.setColor('#1C2337')
-					.setURL(json.permalink)
-					.setFooter(json.author)
-					.setThumbnail('https://allstaractivist.files.wordpress.com/2015/02/urban_dict.png?w=627&h=214')
-					.addField('👍 Likes',json.thumbs_up,true)
-					.addField('👎 Dislikes',json.thumbs_down,true);
-				if (definition) {
-					message.channel.send(definition)
-				} else {
-					message.channel.send("There are no Urban Dictionary ")
-				}
+        if (json) {
+          const definition = new Discord.RichEmbed()
+            .setTitle(json.word)
+            .setDescription(json.definition.replace(/\[\]/g,''))
+            .addField('Example:',json.example.replace(/\[\]/g,''))
+            .setTimestamp(json.written_on.substring(0,json.written_on.indexOf('T')-1).replace(/-/gi,' '))
+            .setColor('#1C2337')
+            .setURL(json.permalink)
+            .setFooter(json.author)
+            .setThumbnail('https://allstaractivist.files.wordpress.com/2015/02/urban_dict.png?w=627&h=214')
+            .addField('👍 Likes',json.thumbs_up,true)
+            .addField('👎 Dislikes',json.thumbs_down,true);
+
+          message.channel.send(definition);
+        } else {
+          message.channel.send(`There is no entry for ${args.join(' ')}`);
+        };
 			});
 		} else {
 			urban.random().first(function(json) {
 				const definition = new Discord.RichEmbed()
 					.setTitle(json.word)
-					.addField('Definition:',json.definition.replace(/\[\]/g,''))
+					.setDescription(json.definition.replace(/\[\]/g,''))
 					.addField('Example:',json.example.replace(/\[\]/g,''))
 					.setTimestamp(json.written_on.substring(0,json.written_on.indexOf('T')-1).replace(/-/gi,' '))
 					.setColor('#1C2337')
@@ -43,7 +44,7 @@ module.exports = {
 					.addField('👍 Likes',json.thumbs_up,true)
 					.addField('👎 Dislikes',json.thumbs_down,true);
 
-				message.channel.send(definition)
+				message.channel.send(definition);
 			});
 		};
 	},
